427d5db604b63e54f5ee410bcf5ea5510ab02187
あいか <47346134+RubyHiroki@users.noreply.github.com>
2019-05-09 09:46:17 +0900

    Add files via upload
    じゃんけんプログラムのコミット

--- janken.rb
+++ janken.rb
@@ -1,10 +1,53 @@
-require 'dxruby'
+puts "じゃんけんをします"
+puts "1がグー、2がチョキ、3がパー"
+# 改行
+print "\n" 
+print "1, 2, 3のどれかを入力してください : "
 
-# 背景画像の準備
-background = Image.load("image/syogiita.jpg")
+# 入力させる
+user_select = gets.to_i
 
-# ウィンドウの生成
-Window.loop do
-# 背景画像の出力
-  Window.draw_scale(0, 0, background, 5, 3.5)
+# aという配列に1, 2, 3を準備
+a = [1, 2, 3]
+
+# sampleメソッドを使って配列の中身をランダムに取得
+com_select = a.sample
+
+# case式でそれぞれ判定させる
+case user_select
+when 1
+  case com_select
+  when 1
+    puts "あいこ"
+  when 2
+    puts "あなたの勝ち"
+  when 3
+    puts "あなたの負け"
+  end
+
+when 2
+  case com_select
+  when 1
+    puts "負け"
+  when 2
+    puts "あなたのあいこ"
+  when 3
+    puts "あなたの勝ち"
+  end
+
+when 3
+  case com_select
+  when 1
+    puts "あなたの勝ち"
+  when 2
+    puts "あなたの負け"
+  when 3
+    puts "あいこ"
+  end
+
+#   1, 2, 3以外の値が入力されたときの出力
+else
+    puts "判定できません"
 end
+
+aaa
